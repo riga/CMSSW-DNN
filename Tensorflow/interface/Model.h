6327df76cf470f8bc3452fddf9458d8579cb0503
Marcel Rieger <marcel.rieger@cern.ch>
2017-04-28 21:14:04 +0200

    Create first embedded tensorflow script.

--- Tensorflow/interface/Model.h
+++ Tensorflow/interface/Model.h
@@ -1,5 +1,8 @@
 /*
  * Generic Tensorflow model.
+ *
+ * Author:
+ *   Marcel Rieger
  */
 
 #ifndef DNN_TENSORFLOW_MODEL_H
@@ -13,6 +16,59 @@
 
 namespace DNN
 {
+
+class Model {
+public:
+    Model();
+    virtual ~Model();
+
+    void loadModel();
+
+    void defineInputs(const std::vector<std::string>& inputs);
+    void defineOutputs(const std::vector<std::string>& outputs);
+
+private:
+    PythonInterface* python;
+
+    size_t nInputs;
+    size_t nOutputs;
+};
+
+static std::string embeddedTensorflowScript = "\
+import os, sys, numpy as np\n\
+tf = saver = graph = sess = inputs = outputs = None\n\
+\n\
+def insert_path(path):\n\
+    path = os.path.expandvars(os.path.expanduser(path))\n\
+    sys.path.append(path)\n\
+\n\
+def import_tf():\n\
+    global tf\n\
+    import tensorflow as tf\n\
+\n\
+def load_graph(path):\n\
+    global saver, graph\n\
+    path = os.path.expandvars(os.path.expanduser(path))\n\
+    saver = tf.train.import_meta_graph(path)\n\
+    graph = tf.get_default_graph()\n\
+\n\
+def get_inputs(*names):\n\
+    global inputs\n\
+    inputs = [graph.get_tensor_by_name(name) for name in names]\n\
+\n\
+def get_outputs(*names):\n\
+    global outputs\n\
+    outputs = [graph.get_tensor_by_name(name) for name in names]\n\
+\n\
+def start_session():\n\
+    global sess\n\
+    sess = tf.Session()\n\
+    sess.run(tf.global_variables_initializer())\n\
+\n\
+def call(*values):\n\
+    results = sess.run(inputs, feed_dict=dict(zip(inputs, values)))\n\
+    return len(results)\n\
+";
 
 } // namepace DNN
 
--- Tensorflow/src/Model.cc
+++ Tensorflow/src/Model.cc
@@ -1,10 +1,40 @@
 /*
  * Generic Tensorflow model.
+ *
+ * Author:
+ *   Marcel Rieger
  */
 
 #include "DNN/Tensorflow/interface/Model.h"
 
 namespace DNN
 {
+
+Model::Model()
+    : python(0)
+    , nInputs(0)
+    , nOutputs(0)
+{
+    python = new PythonInterface();
+    python->runScript(embeddedTensorflowScript);
+}
+
+Model::~Model()
+{
+    if (python)
+    {
+        delete python;
+    }
+}
+
+void Model::defineInputs(const std::vector<std::string>& inputs)
+{
+
+}
+
+void Model::defineOutputs(const std::vector<std::string>& outputs)
+{
+    
+}
 
 } // namespace DNN
