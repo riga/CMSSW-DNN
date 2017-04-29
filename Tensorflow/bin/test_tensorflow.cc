8a513bd0be37bdadbf7a00049165a6ce6165fbe9
Marcel Rieger <marcel.rieger@cern.ch>
2017-04-29 13:33:07 +0200

    Update tensorflow test.

--- Tensorflow/bin/test_tensorflow.cc
+++ Tensorflow/bin/test_tensorflow.cc
@@ -1,6 +1,12 @@
 /*
  * Simple test of the tensorflow interface.
  *
+ * Graph (from data/simplegraph.pb, created by test/creategraph.py):
+ *   Input:
+ *     - name = "input:0", shape = (batch, 10)
+ *   Output:
+ *     - name = "output:0", shape = (batch, 1)
+ *
  * Usage:
  *   > test_tensorflow
  *
@@ -9,6 +15,8 @@
  */
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "DNN/Tensorflow/interface/Graph.h"
 
@@ -16,13 +24,20 @@ int main(int argc, char* argv[])
 {
     std::cout << "tensorflow test" << std::endl;
 
+    // get the file containing the graph
     std::string cmsswBase = std::string(getenv("CMSSW_BASE"));
     std::string dataDir = cmsswBase + "/src/DNN/Tensorflow/data";
     std::string graphFile = dataDir + "/simplegraph.pb";
     std::cout << "load graph " << graphFile << std::endl;
 
-    DNN::TensorflowGraph g();
+    // load and initialize graph
+    DNN::TensorflowGraph g(graphFile, DNN::LogLevel::ALL);
+    g.defineInputs({"input:0"});
+    g.defineOutputs({"output:0"});
+    g.startSession();
 
+    // evaluation calls
+    // TODO
 
     return 0;
 }
