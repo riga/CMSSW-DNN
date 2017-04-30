ff4decfe8d49514f7975d4e25f644c9d77b1a154
Marcel Rieger <marcel.rieger@cern.ch>
2017-04-30 18:03:33 +0200

    Update format of saved model, update python test scripts.

--- Tensorflow/test/creategraph.py
+++ Tensorflow/test/creategraph.py
@@ -7,13 +7,16 @@ thisdir = os.path.dirname(os.path.abspath(__file__))
 datadir = os.path.join(os.path.dirname(thisdir), "data")
 
 x_ = tf.placeholder(tf.float32, [None, 10], name="input")
-W = tf.Variable(tf.ones([10, 1]))
-b = tf.Variable(tf.ones([1]))
+W = tf.Variable(tf.ones([10, 1]), name="weights")
+b = tf.Variable(tf.ones([1]), name="biases")
 y = tf.add(tf.matmul(x_, W), b, name="output")
 
+init_op = tf.global_variables_initializer()
+
 sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+sess.run(init_op)
 
 print(sess.run(y, feed_dict={x_: [range(10)]})[0][0])
 
-tf.train.export_meta_graph(os.path.join(datadir, "simplegraph.pb"))
+saver = tf.train.Saver()
+saver.save(sess, os.path.join(datadir, "simplegraph"))
--- Tensorflow/test/restoregraph.py
+++ Tensorflow/test/restoregraph.py
@@ -6,13 +6,9 @@ import tensorflow as tf
 thisdir = os.path.dirname(os.path.abspath(__file__))
 datadir = os.path.join(os.path.dirname(thisdir), "data")
 
-saver = tf.train.import_meta_graph(os.path.join(datadir, "simplegraph.pb"))
-graph = tf.get_default_graph()
-
-x_ = graph.get_tensor_by_name("input:0")
-y = graph.get_tensor_by_name("output:0")
-
 sess = tf.Session()
-sess.run(tf.global_variables_initializer())
 
-print(sess.run(y, feed_dict={x_: [range(10)]})[0][0])
+saver = tf.train.import_meta_graph(os.path.join(datadir, "simplegraph.meta"))
+saver.restore(sess, os.path.join(datadir, "simplegraph"))
+
+print(sess.run({"y": "output:0"}, feed_dict={"input:0": [range(10)]})["y"][0][0])
