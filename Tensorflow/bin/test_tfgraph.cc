ffa812dc4da92ae130b3dfabefc20d778024a741
Marcel Rieger <marcel.rieger@cern.ch>
2017-04-30 18:05:57 +0200

    Update test binaries.

---
+++ Tensorflow/bin/test_tfgraph.cc
@@ -0,0 +1,85 @@
+/*
+ * Simple test of the tensorflow graph interface.
+ *
+ * Graph (from data/simplegraph.pb, created by test/creategraph.py):
+ *   Input:
+ *     - name = "input:0", shape = (batch, 10)
+ *   Output:
+ *     - name = "output:0", shape = (batch, 1)
+ *
+ * Usage:
+ *   > test_tfgraph
+ *
+ * Author:
+ *   Marcel Rieger
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "DNN/Tensorflow/interface/Tensor.h"
+#include "DNN/Tensorflow/interface/Graph.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << std::endl << "test dnn::tf::Graph" << std::endl;
+
+    // get the file containing the graph
+    std::string cmsswBase = std::string(getenv("CMSSW_BASE"));
+    std::string dataDir = cmsswBase + "/src/DNN/Tensorflow/data";
+    std::string graphFile = dataDir + "/simplegraph";
+    std::cout << "load graph " << graphFile << std::endl;
+
+
+    //
+    // object definitions
+    //
+
+    // load and initialize graph
+    dnn::tf::Graph g(graphFile, dnn::LogLevel::ALL);
+
+    // prepare input and output tensors
+    npy_intp xShape[] = {1, 10};
+    dnn::tf::Tensor* x = g.defineInput(new dnn::tf::Tensor("input:0", 2, xShape));
+    dnn::tf::Tensor* y = g.defineOutput(new dnn::tf::Tensor("output:0"));
+
+
+    //
+    // evaluation
+    //
+
+    // fill a single batch of the input tensor with consecutive numbers
+    for (int i = 0; i < x->getShape(1); i++)
+    {
+        x->setValue<float>(0, i, (float)i);
+    }
+
+    // evaluation call
+    // this does not return anything but changes the output tensor(s) in place which is faster
+    g.eval();
+
+    // some outputs
+    std::cout << "rank : " << y->getRank() << std::endl;
+    std::cout << "shape: ";
+    for (int i = 0; i < y->getRank(); i++)
+    {
+        std::cout << y->getShape(i) << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "value: " << y->getValue<float>(0, 0) << std::endl;
+
+
+    //
+    // cleanup
+    //
+
+    // remove tensors manually (you type 'new', you type 'delete')
+    delete x;
+    delete y;
+
+
+    std::cout << std::endl << "done" << std::endl;
+
+    return 0;
+}
