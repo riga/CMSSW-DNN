93f98736dc0c491738e6ad65901827ee753bab55
Marcel Rieger <marcel.rieger@cern.ch>
2017-05-03 16:28:23 +0200

    Add custom py2-numpy-c-api tool file as a temporary fix.

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ action() {
 		# setup the tensorflow software bundle, only if the CMSSW major version is <= 8
 		if [[ "$( echo $CMSSW_VERSION | cut -d_ -f2 )" -le 8 ]]; then
 			echo "setup tensorflow bundle"
-			cd $CMSSW_BASE/src/DNN/Tensorflow
+			cd "$CMSSW_BASE/src/DNN/Tensorflow"
 			if [ -f "/afs/cern.ch/work/m/mharrend/public/tensorflow-cmssw8-0-26.tar.gz" ]; then
 				echo "fetch from /afs"
 				cp /afs/cern.ch/work/m/mharrend/public/tensorflow-cmssw8-0-26.tar.gz .
@@ -24,8 +24,19 @@ action() {
 			mkdir -p python
 			mv tensorflow-cmssw8-0-26-patch1/site-packages/* python/
 			rm -rf tensorflow-cmssw8-0-26.tar.gz tensorflow-cmssw8-0-26-patch1/
+			cd "$origin"
 			echo "tensorflow bundle setup successful"
 		fi
+
+		# setup the custom numpy c-api tool file
+		# this is a temporary fix until it was centrally deployed by the cmsdist team
+		# see https://github.com/cms-sw/cmsdist/issues/2994
+		echo "setup tool for numpy C API"
+		cd "$CMSSW_BASE/src"
+		scram setup "DNN/Tensorflow/py2-numpy-c-api.xml"
+		eval `scramv1 runtime -sh`
+		cd "$origin"
+		echo "numpy C API tool setup successful"
 
 	else
 		echo "please setup CMSSW and do 'cmsenv' before calling this script"
