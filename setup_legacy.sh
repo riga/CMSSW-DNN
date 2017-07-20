90c7a5a01d09ae45ba24bac5a652913f075683a8
Marcel Rieger <marcel.rieger@cern.ch>
2017-07-20 15:52:04 +0200

    Add switch for SCRAM_ARCH and CMSSW_VERSION in setup script.

---
+++ setup_legacy.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Legacy setup script for slc6_amd64_gcc530 and 80X or <= 92X.
+# Usage:
+#   > ./setup.sh
+
+action() {
+	local origin="$( /bin/pwd )"
+
+	if [ -d "$CMSSW_BASE" ]; then
+
+		# check SCRAM_ARCH
+		if [ "$( echo $SCRAM_ARCH | cut -d_ -f3 )" != "gcc530" ]; then
+			>&2 echo "compiler in SCRAM_ARCH '$SCRAM_ARCH' but must be gcc530"
+			cd "$origin"
+			return 1
+		fi
+
+		# check CMSSW_VERSION
+		local cv="$( echo $CMSSW_VERSION | cut -d_ -f2-3 )"
+		if [ "$cv" != "8_0" ] && [ "$cv" != "9_0" ] && [ "$cv" != "9_1" ] && [ "$cv" != "9_2" ]; then
+			>&2 echo "CMSSW_VERSION '$CMSSW_VERSION' not supported"
+			cd "$origin"
+			return 1
+		fi
+
+		# download and unpack the bundle
+		echo "setup tensorflow bundle"
+		cd "$CMSSW_BASE/src/DNN/Tensorflow"
+		if [ -f "/afs/cern.ch/work/m/mharrend/public/tensorflow-cmssw8-0-26.tar.gz" ]; then
+			echo "fetch from /afs"
+			cp /afs/cern.ch/work/m/mharrend/public/tensorflow-cmssw8-0-26.tar.gz .
+		else
+			echo "download"
+			wget -nv http://www-ekp.physik.uni-karlsruhe.de/~harrendorf/tensorflow-cmssw8-0-26.tar.gz
+		fi
+		tar -zxf tensorflow-cmssw8-0-26.tar.gz
+		mkdir -p python
+		mv tensorflow-cmssw8-0-26-patch1/site-packages/* python/
+		rm -rf tensorflow-cmssw8-0-26.tar.gz tensorflow-cmssw8-0-26-patch1/
+		cd "$origin"
+		echo "tensorflow bundle setup successful"
+
+		# setup the custom numpy c-api tool file
+		# this is a temporary fix until it was centrally deployed by the cmsdist team
+		# see https://github.com/cms-sw/cmsdist/issues/2994
+		echo "setup tool for numpy C API"
+		cd "$CMSSW_BASE/src"
+		scram setup "DNN/py2-numpy-c-api.xml"
+		eval `scramv1 runtime -sh`
+		cd "$origin"
+		echo "numpy C API tool setup successful"
+
+	else
+		>&2 echo "please setup CMSSW before calling this script"
+		cd "$origin"
+		return 1
+	fi
+}
+action "$@"
