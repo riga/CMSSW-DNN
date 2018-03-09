1449e568a78be2051d5d2b616ce1c999a7822ed2
Marcel Rieger <marcel.rieger@cern.ch>
2018-03-09 19:16:05 +0100

    Change custom sessions and ci script.

--- TensorFlow/src/TBBSession.h
+++ TensorFlow/src/TBBSession.h
@@ -13,6 +13,26 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+/*
+This file is an adaptation of the original direct_session.h file located at
+https://github.com/tensorflow/tensorflow/blob/v1.5.0/tensorflow/core/common_runtime/direct_session.h
+to meet the demands of the software environment developed and used by the CMS collaboration.
+
+Changes:
+  - Renamed the session class to TBBSession (TBB = thread building blocks)
+  - Renamed some members to refelct that change
+  - Removed the thread_pools_ member
+  - Set the session handle to "tbb"
+  - Removed the PRunSetup, PRun, SendPRunInputs, RecvPRunOutputs and CheckFetch methods
+  - Removed the ThreadPool arguments from GetOrCreateExecutors and SchedClosure
+  - Let tbb arena and tbb group handle scheduling in WaitForNotification and SchedClosure
+  - Removed obsolete helper functions NumInterOpThreadsFromSessionOptions,
+    NewThreadPoolFromSessionOptions, NewThreadPoolFromThreadPoolOptions and GlobalThreadPool
+  - Renamed the session factory class to TBBSessionFactory
+  - Renamed the session registrar class to TBBSessionRegistrar
+  - Renamed include guard to reflect location within CMSSW
+*/
+
 #ifndef PHYSICSTOOLS_TENSORFLOW_TBBSESSION_H
 #define PHYSICSTOOLS_TENSORFLOW_TBBSESSION_H
 
@@ -30,9 +50,10 @@ limitations under the License.
 #include "tensorflow/core/common_runtime/device_mgr.h"
 #include "tensorflow/core/common_runtime/device_set.h"
 #include "tensorflow/core/common_runtime/executor.h"
+#include "tensorflow/core/common_runtime/graph_execution_state.h"
+#include "tensorflow/core/common_runtime/process_function_library_runtime.h"
 #include "tensorflow/core/common_runtime/rendezvous_mgr.h"
 #include "tensorflow/core/common_runtime/session_factory.h"
-#include "tensorflow/core/common_runtime/simple_graph_execution_state.h"
 #include "tensorflow/core/framework/cancellation.h"
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/session_state.h"
@@ -45,8 +66,10 @@ limitations under the License.
 #include "tensorflow/core/public/session.h"
 
 namespace tbb {
+
 class task_group;
-}
+
+}  // end namespace tbb
 
 namespace tensorflow {
 
@@ -68,8 +91,7 @@ class TBBSession : public Session {
   ~TBBSession() override;
 
   typedef std::vector<std::pair<string, Tensor>> NamedTensorList;
-  typedef std::unordered_map<StringPiece, Node*, StringPiece::Hasher>
-      NameNodeMap;
+  typedef std::unordered_map<StringPiece, Node*, StringPieceHasher> NameNodeMap;
 
   ::tensorflow::Status Create(const GraphDef& graph) override;
   ::tensorflow::Status Extend(const GraphDef& graph) override;
@@ -86,7 +108,6 @@ class TBBSession : public Session {
                            std::vector<Tensor>* outputs,
                            RunMetadata* run_metadata) override;
 
-
   // Reset clears 'containers' from the device_mgr of the TBBSession.
   // If 'containers' is empty, then Reset clears the default container.
   ::tensorflow::Status Reset(const std::vector<string>& containers);
@@ -94,19 +115,22 @@ class TBBSession : public Session {
   ::tensorflow::Status ListDevices(
       std::vector<DeviceAttributes>* response) override;
   ::tensorflow::Status Close() override;
+  ::tensorflow::Status LocalDeviceManager(const DeviceMgr** output) override {
+    *output = device_mgr_.get();
+    return ::tensorflow::Status::OK();
+  }
 
   void ExportCostModels(CostModelManager::CostModelMap* cost_models) {
     cost_model_manager_.ExportCostModels(cost_models);
   }
 
  private:
-  typedef TBBSession ME;
-
   // We create one executor and its dependent library runtime for
   // every partition.
   struct PerPartitionExecutorsAndLib {
-    Graph* graph = nullptr;
-    std::unique_ptr<FunctionLibraryRuntime> flib;
+    Graph* graph = nullptr;                  // not owned.
+    Device* device = nullptr;                // not owned.
+    FunctionLibraryRuntime* flib = nullptr;  // not owned.
     std::unique_ptr<Executor> executor;
   };
 
@@ -119,6 +143,8 @@ class TBBSession : public Session {
   // 'input_keys' are the rendezvous keys for the feeds and 'output_keys'
   // are rendezvous keys for the fetches.
   // 'flib_def' is the function library used by graphs in 'items'.
+  // 'proc_flr' is the collection of FunctionLibraryRuntime objects, one per
+  // device.
   // TODO(phawkins): currently partitions always share the same function
   // library. Consider giving each partition its own function library to enable
   // per-partition rewrites.
@@ -129,6 +155,7 @@ class TBBSession : public Session {
     std::unique_ptr<Graph> graph;
     NameNodeMap name_to_node;
     std::unique_ptr<FunctionLibraryDefinition> flib_def;
+    std::unique_ptr<ProcessFunctionLibraryRuntime> proc_flr;
     std::vector<PerPartitionExecutorsAndLib> items;
     std::unordered_map<string, size_t> input_name_to_index;
     std::unordered_map<string, string> input_name_to_rendezvous_key;
@@ -184,8 +211,8 @@ class TBBSession : public Session {
   // Retrieves an already existing set of executors to run 'inputs' and
   // 'outputs', or creates and caches them for future use.
   ::tensorflow::Status GetOrCreateExecutors(
-      gtl::ArraySlice<string> inputs,
-      gtl::ArraySlice<string> outputs, gtl::ArraySlice<string> target_nodes,
+      gtl::ArraySlice<string> inputs, gtl::ArraySlice<string> outputs,
+      gtl::ArraySlice<string> target_nodes,
       ExecutorsAndKeys** executors_and_keys, RunStateArgs* run_state_args);
 
   // Creates several graphs given the existing graph_def_ and the
@@ -210,9 +237,8 @@ class TBBSession : public Session {
   // If the timeout expires, the `cm->StartCancel()` will be called.
   ::tensorflow::Status WaitForNotification(Notification* n,
                                            int64 timeout_in_ms);
-  void WaitForNotification(tbb::task_arena& arena, tbb::task_group& group,  
-                           RunState* run_state, CancellationManager* cm,
-                           int64 timeout_in_ms);
+  void WaitForNotification(tbb::task_arena& arena, tbb::task_group& group,
+      RunState* run_state, CancellationManager* cm, int64 timeout_in_ms);
 
   ::tensorflow::Status CheckNotClosed() {
     mutex_lock l(closed_lock_);
@@ -276,7 +302,7 @@ class TBBSession : public Session {
       GUARDED_BY(graph_def_lock_);
 
   // Execution_state; used when placing the entire graph.
-  std::unique_ptr<SimpleGraphExecutionState> execution_state_
+  std::unique_ptr<GraphExecutionState> execution_state_
       GUARDED_BY(graph_def_lock_);
 
   // The function library, before any rewrites or optimizations have been
